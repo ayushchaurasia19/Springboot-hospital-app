3120020595b662d3e9939da39b2f08b02bfd4fec
Ayush <ayushin321@gmail.com>
2026-05-21 23:21:35 +0530

    comments

--- src/test/java/com/hospital/hospital_api/PatientControllerTest.java
+++ src/test/java/com/hospital/hospital_api/PatientControllerTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc // fake HTTP requests for testing
 public class PatientControllerTest {
 
     @Autowired
